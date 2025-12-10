c6cd711248d436aeb0e798946dc6064516ca9a1b
root <cooperflowers@icloud.com>
2025-12-09 23:25:30 -0800

    final wordle and schedwork submission

--- schedwork.cpp
+++ schedwork.cpp
@@ -14,13 +14,17 @@
 
 using namespace std;
 
-// a constant that can be used to indicate an INVALID 
-// worker ID if that is useful to your implementation.
-// Feel free to not use or delete.
-static const Worker_T INVALID_ID = (unsigned int)-1;
+
 
 
 // Add prototypes for any helper functions here
+
+
+// be recursive, fill the schedule slots // have current day, id for how many needed filled, each workers' shift count
+bool help(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, std::vector<size_t>& shiftCount, size_t current, size_t id);
+
+
+
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -40,6 +44,84 @@ bool schedule(
 
 
 
+    const size_t n = avail.size();          //number of days
+    const size_t k = avail[0].size();       //number of workers
+
+    if(k == 0 || dailyNeed == 0) {          //no workers or needed workers means no schedule
+        return false;
+    }
+
+   
+    if(dailyNeed > k) {                        //impossible case, saves checking everythin else          
+        return false;
+    }
+
+    
+    sched.clear();                                      //new sched
+    for(size_t i = 0; i < n; ++i) {        
+        std::vector<Worker_T> row(dailyNeed, 0);            //fill it up with empty "0"
+        sched.push_back(row);                           //we get n x d daily needed
+    }
+
+    
+    std::vector<size_t> shiftCount(k, 0);               //keep track of each workers shift count
+
+    
+    return help(avail, dailyNeed, maxShifts, sched, shiftCount, 0, 0);          //start recursing
+
 
 }
 
+
+bool help(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, std::vector<size_t>& shiftCount, size_t current, size_t id){
+    
+    
+    const size_t n = avail.size();
+    const size_t k = avail[0].size();
+
+   
+    if(current == n) {                      //all days scheduled
+        return true;
+    }
+
+    
+    if(id == dailyNeed) {                                                              //filled current day, move on
+        return help(avail, dailyNeed, maxShifts, sched, shiftCount, current + 1, 0);
+    }
+
+    
+    for(size_t w = 0; w < k; ++w) {                 //loop to go thru all workers to check availability
+
+        if(!avail[current][w]) {                //check if worker is available
+            continue;
+        }
+
+        if(shiftCount[w] >= maxShifts) {        //check if they have not exceeded max shifts
+            continue;
+        }
+
+        bool workCheck = false;                 //need to check to prevent same person two shift slots
+        for(size_t j = 0; j < id; ++j) {
+            if(sched[current][j] == w) {
+                workCheck = true;
+                break;
+            }
+        }
+        if(workCheck) {
+            continue;
+        }
+        
+        sched[current][id] = w;                 //worker is valid so assign to the shift
+        shiftCount[w]++;                        //add to their shift count
+
+        if(help(avail, dailyNeed, maxShifts, sched, shiftCount, current, id + 1)) {     //do next slot in shift, if all can be filled returns true
+            return true;
+        }
+
+        sched[current][id] = 0;             //when above is false, we undo the assignment and redo to find a different worker
+        shiftCount[w]--;
+    }
+
+    
+    return false;               //no worker can create a valid schedule
+}
--- wordle.cpp
+++ wordle.cpp
@@ -15,6 +15,12 @@ using namespace std;
 // Add prototypes of helper functions here
 
 
+
+bool fixCheck(const std::string& word, const std::string& in, int wid);       //check matching fixed letters
+bool floatCheck(const std::string& word, int wid, const std::string& remaining);   //check if all floating letters appear //after fix check
+
+
+
 // Definition of primary wordle function
 std::set<std::string> wordle(
     const std::string& in,
@@ -23,6 +29,88 @@ std::set<std::string> wordle(
 {
     // Add your code here
 
+    std::set<std::string> valid;         // set of all valid words
+
+    
+    for (std::set<std::string>::const_iterator it = dict.begin();       //for for all dict words
+         it != dict.end();
+         ++it)
+    {
+        const std::string& word = *it;     // current dictionary word on current iterator
+
+        if (word.size() != in.size()) {     //check for same size
+            continue;
+        }
+
+        bool lower = true;
+        for (size_t i = 0; i < word.size(); ++i) {      // checking lowercase
+            if (word[i] < 'a' || word[i] > 'z') {
+                lower = false;
+                break;
+            }
+        }
+        if (!lower) {
+            continue;                               //won't consider uppercase
+        }
+
+
+        if (!fixCheck(word, in, 0)) {          //check fixed slots word to in
+            continue;
+        }
+
+        if (!floatCheck(word, 0, floating)) {      //checks floating slots, do the floating letters appear anywhere
+            continue;
+        }
+
+        valid.insert(word);
+    }
+
+    return valid;
+
+
+    
 }
 
 // Define any helper functions here
+
+
+
+
+
+bool fixCheck(const std::string& word, const std::string& in, int wid){
+
+    if (wid == (int)in.size()) {                  //base case when reached end
+        return true;
+    }
+
+    if (in[wid] != '-' && word[wid] != in[wid]) {             //position has a letter that does not match or is not -, false
+        return false;
+    }
+
+    return fixCheck(word, in, wid + 1);           //move on
+}
+
+
+
+bool floatCheck(const std::string& word, int wid, const std::string& remaining){            //checks for remaining floating letters
+
+
+    if (wid == (int)word.size()) {                  //end of word base case
+        return remaining.empty();                   //false if not all floating were found
+    }
+
+    char c = word[wid];                  // current 
+
+    for (size_t j = 0; j < remaining.size(); ++j) {         //loop to check if c matches any remaining
+        
+        if (remaining[j] == c) {                            //match found
+           
+            std::string next = remaining;               //copy remaining
+            next.erase(j, 1);              
+
+            return floatCheck(word, wid + 1, next);     //recurse to check next character
+        }
+    }
+    
+    return floatCheck(word, wid + 1, remaining);
+}
